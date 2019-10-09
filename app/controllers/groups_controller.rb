5f333a1c104a85b8cc0c4dc0c555b9c6a25cc653
kenta-m821 <kenta.m821@gmail.com>
2019-10-09 20:19:27 +0900

    add incremental

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,14 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
@@ -26,14 +34,6 @@ class GroupsController < ApplicationController
   def edit
   end
 
-  def update
-    if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-    else
-      render :edit
-    end
-  end
-  
   def set_group
     @group = Group.find(params[:id])
   end
